eabab2955a2d934083139bc1a2ed3276a7b75b83
Manon Berne <mberne@z1r6p3.42lyon.fr>
2021-11-02 17:24:49 +0100

    exit full fix and export/env back

--- srcs/builtins.c
+++ srcs/builtins.c
@@ -6,7 +6,7 @@
 /*   By: mberne <mberne@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 16:01:23 by mberne            #+#    #+#             */
-/*   Updated: 2021/11/02 17:06:37 by mberne           ###   ########lyon.fr   */
+/*   Updated: 2021/11/02 17:24:21 by mberne           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -113,27 +113,20 @@ void	bi_exit(t_structs *s, t_cmd current)
 		free_all(s);
 		exit(errno);
 	}
-	else if (current.cmd[2])
+	if (current.cmd[2])
+	{
 		print_error(NULL, "exit", "too many arguments\n", EXIT_FAILURE);
+		return ;
+	}
 	else if ((current.cmd[1][0] == '-' || ft_isdigit(current.cmd[1][0]))
 		&& ft_str_isdigit(current.cmd[1] + 1))
-	{
 		errno = ft_atoi(current.cmd[1]);
-		free_all(s);
-		exit(errno);
-	}
 	else if (ft_str_isprint(current.cmd[1])
 		&& !ft_atoi_exit(current.cmd[1], &exit_status))
-	{
 		errno = exit_status;
-		free_all(s);
-		exit(errno);
-	}
 	else
-	{
 		print_error("exit: ", current.cmd[1],
 			"numeric argument required\n", 255);
-		free_all(s);
-		exit(errno);
-	}
+	free_all(s);
+	exit(errno);
 }
