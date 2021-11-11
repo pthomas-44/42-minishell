10d2686e50fa7147015910bb742b7032b7c587dc
Pierre Thomas <pthomas@student.42lyon.fr>
2021-11-11 11:25:11 +0100

    fix error msg on cd

--- src/builtin/bi_cd.c
+++ src/builtin/bi_cd.c
@@ -6,7 +6,7 @@
 /*   By: pthomas <pthomas@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 13:43:33 by mberne            #+#    #+#             */
-/*   Updated: 2021/11/10 14:41:14 by pthomas          ###   ########lyon.fr   */
+/*   Updated: 2021/11/11 10:36:53 by pthomas          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,7 +101,7 @@ void	bi_cd(t_structs *s, t_cmd *current)
 		current->path = ft_strdup(current->cmd[1]);
 	if (current->path && chdir(current->path) == -1)
 	{
-		print_error("cd: ", current->path, NULL, ENOENT);
+		print_error("cd: ", current->path, NULL, errno);
 		g_numberr = EXIT_FAILURE;
 	}
 	else if (!current->path || set_pwd(s, cwd) == -1)
